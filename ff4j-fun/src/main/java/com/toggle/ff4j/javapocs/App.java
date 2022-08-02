17ecca78a0d5b4ccb2f6c928194157803c74bd33
SK <sudhirid@gmail.com>
2022-08-02 09:02:52 -0500

    Add files via upload

---
+++ ff4j-fun/src/main/java/com/toggle/ff4j/javapocs/App.java
@@ -0,0 +1,26 @@
+package com.toggle.ff4j.javapocs;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.toggle.ff4j.javapocs")
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("Spring Boot 2.0 working! ");
+		};
+	}
+}
---
+++ ff4j-sample-client/src/test/java/org/ff4j/test/SampleFF4jClientTest.java
@@ -0,0 +1,57 @@
+package org.ff4j.test;
+
+import java.util.Map;
+
+import org.ff4j.FF4j;
+import org.ff4j.cache.FF4jCacheProxy;
+import org.ff4j.cache.InMemoryCacheManager;
+import org.ff4j.core.Feature;
+import org.ff4j.web.jersey2.store.FeatureStoreHttp;
+import org.ff4j.web.jersey2.store.PropertyStoreHttp;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class SampleFF4jClientTest {
+    
+    private static FF4j ff4j;
+    
+    @BeforeAll
+    public static void initFF4j() {
+        String ff4jApiEndPoint = "http://localhost:8085/api/ff4j";
+        FeatureStoreHttp  fStore = new FeatureStoreHttp(ff4jApiEndPoint);
+        PropertyStoreHttp pStore = new PropertyStoreHttp(ff4jApiEndPoint);
+        /* 
+         * Maybe we don't want to do an http call each time we test a feature
+         * as there is latency. Local in memory cache with TTL 10min can help.
+         */
+        FF4jCacheProxy cc = new FF4jCacheProxy(fStore, pStore, new InMemoryCacheManager(600));
+
+        ff4j = new FF4j();
+        ff4j.setFeatureStore(cc);
+        ff4j.setPropertiesStore(cc);
+        ff4j.audit(false); //(auditing at server level)
+        ff4j.autoCreate(true);
+    }
+    
+    @Test
+    public void useFF4j() {
+        System.out.println("-----------------------------");
+        System.out.println("--     FF4J FEATURES       --");
+        System.out.println("-----------------------------");
+        Map<String, Feature> allFeatures = ff4j.getFeatureStore().readAll();
+        for(Feature f : allFeatures.values()) {
+            System.out.println("- Feature " + f.getUid() + "=>" + f.toJson());
+        }
+        
+        if (ff4j.check("showUserName")) {
+            System.out.println("'showUserName' is ON");
+        } else {
+            System.out.println("'showUserName' is OFF");
+        }
+        
+        Object s = ff4j.getProperty("username").getValue();
+        System.out.println(s);
+        
+    }
+
+}
